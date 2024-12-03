39436e6dd416e2060e73135ad7c6a3fb6f464aac
Ben Petro <benpetro@gmail.com>
2024-12-03 15:48:53 +1000

    Add context to Situation

--- app/controllers/universal/situations_controller.rb
+++ app/controllers/universal/situations_controller.rb
@@ -1,36 +1,38 @@
-require_dependency "universal/application_controller"
+require_dependency 'universal/application_controller'
 
 module Universal
   class SituationsController < Universal::ApplicationController
-    
     before_action :find_subject
     before_action :find_scope
-    
+
     def create
       if @subject and @scope
-        @subject.situations.create scope: @scope, notes: (params[:situation].blank? ? params[:notes] : params[:situation][:notes])
+        @subject.situations.create scope: @scope,
+                                   notes: (params[:situation].blank? ? params[:notes] : params[:situation][:notes]),
+                                   context: params[:context]
       end
       respond_to do |format|
-        format.json{
+        format.json do
           render json: {}
-        }
-        format.js{
+        end
+        format.js do
           render layout: false
-        }
+        end
       end
     end
-    
+
     private
+
     def find_subject
-      if !params[:subject_type].blank? and !params[:subject_id].blank?
-        @subject = params[:subject_type].classify.constantize.find(params[:subject_id])
-      end
+      return unless !params[:subject_type].blank? and !params[:subject_id].blank?
+
+      @subject = params[:subject_type].classify.constantize.find(params[:subject_id])
     end
+
     def find_scope
-      if !params[:scope_type].blank? and !params[:scope_id].blank?
-        @scope = params[:scope_type].classify.constantize.find(params[:scope_id])
-      end
+      return unless !params[:scope_type].blank? and !params[:scope_id].blank?
+
+      @scope = params[:scope_type].classify.constantize.find(params[:scope_id])
     end
-    
   end
 end
--- lib/universal/models/situation.rb
+++ lib/universal/models/situation.rb
@@ -9,31 +9,32 @@ module Universal
         include Universal::Concerns::Polymorphic
         include Universal::Concerns::Scoped
         include Universal::Concerns::Status
-        
+
         store_in collection: 'universal_situations'
-        
+
+        field :context
         field :n, as: :notes
         field :ea, as: :expires_at, type: DateTime
-        
-        default_scope ->(){where(status: :current, :expires_at.gte => Time.zone.now)}
-        default_scope ->(){order_by(created_at: :desc)}
-        
-        statuses %w(current expired), default: :current
-        
+
+        default_scope -> { where(status: :current, :expires_at.gte => Time.zone.now) }
+        default_scope -> { order_by(created_at: :desc) }
+
+        statuses %w[current expired], default: :current
+
         before_create :set_expiry
         before_create :expire_previous
-        
+
         private
+
         def expire_previous
-          prev = self.subject.situations.where(scope: self.scope).first
-          prev.expired! if !prev.nil?
+          prev = subject.situations.where(scope: scope).first
+          prev.expired! unless prev.nil?
         end
+
         def set_expiry
           self.expires_at = 24.hours.from_now
         end
-        
       end
-
     end
   end
 end
